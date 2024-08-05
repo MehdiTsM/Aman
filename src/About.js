028d65a5b532a26e9cb18fbff931c0374b43a127
Mehdi <mehdiadlen5@gmail.com>
2024-08-05 19:28:12 +0100

    Initial commit

---
+++ src/About.js
@@ -0,0 +1,32 @@
+import React from 'react'
+
+function About() {
+  return (
+    <div className='max-w-3xl mx-auto px-4 py-12 animate-fadeInUp'>
+        <h1 className='font-medium text-xl'>À propos de Aman Taksit :</h1>
+        <br/>
+        <p>Bienvenue chez Aman Taksit, votre partenaire de confiance pour l'achat d'électroménagers par facilité bancaire (morabaha). Nous sommes dédiés à offrir à nos clients des solutions de financement pratiques et transparentes, rendant l'acquisition d'appareils ménagers de qualité accessible à tous.</p>
+        <br />
+        <h1 className='font-medium text-xl'>Notre Mission :</h1>
+        <br />
+        <p>Chez Aman Taksit, nous nous engageons à simplifier le processus d'achat d'électroménagers en proposant des options de paiement adaptées à chaque budget. Notre objectif est de permettre à chaque foyer d'accéder aux meilleures marques d'électroménagers, tout en bénéficiant de conditions financières avantageuses.</p>
+        <br />
+        <h1 className='font-medium text-xl'>Ce que Nous Offrons :</h1>
+        <br />
+        <p>Une large gamme d'électroménagers de marques réputées.
+        Des solutions de financement par facilité bancaire (morabaha) pour des paiements échelonnés.
+        Un service client dévoué et attentif à vos besoins.
+        </p>
+        <br />
+        <h1 className='font-medium text-xl'>Pourquoi Choisir Aman Taksit ?</h1>
+        <br/>
+        <p>Fiabilité : Nous travaillons avec des banques partenaires pour offrir des financements sûrs et conformes aux principes de morabaha.
+Qualité : Nos produits sont soigneusement sélectionnés pour garantir une performance et une durabilité exceptionnelles.
+Accessibilité : Grâce à nos options de paiement flexibles, équipez votre maison sans contrainte financière.
+Rejoignez la famille Aman Taksit et découvrez une nouvelle façon d'acheter vos électroménagers en toute sérénité.</p>
+
+    </div>
+  )
+}
+
+export default About
